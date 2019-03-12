84f2d48a519877da554e3dbdc1e1647d59757e5e
Loredana Iosub <loredana.iosub28@gmail.com>
2019-03-12 22:32:05 +0200

    first commit

--- pvpt/Controllers/HomeController.cs
+++ pvpt/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace pvpt.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {//comentariu
             return View();
         }
 
